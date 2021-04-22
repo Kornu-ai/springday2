a56b8cda6a758b8de914e0d7e02bd3157ed1d370
babji <babji@DESKTOP-2VFRJ0D>
2021-04-22 11:32:12 +0530

    Initial commit

---
+++ springs2/src/basics/annotations/Car.java
@@ -0,0 +1,29 @@
+package basics.annotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements Vehicle{
+	public Car() {
+		System.out.println("Car- constructor");
+	}
+	
+	@Autowired
+	Tyre tyre;
+	
+	
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
+	public void drive() {
+		System.out.println("driving car with tyre"+tyre);
+	}
+
+}
